8f490197028159182067a2f5c68b3c8282fb81ad
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-04-19 17:16:42 -0400

    finished account ledgers

--- client/src/components/AccountLedgers/AccountsReceivable.js
+++ client/src/components/AccountLedgers/AccountsReceivable.js
@@ -1,29 +1,209 @@
 import React from 'react';
+import ReactTable from 'react-table';
 
 class AccountsReceivableLedger extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            transactions: []
+            transactions: "",
+            data: [],
+            balance: {}
+        }
+
+        this.getDebits = this.getDebits.bind(this);
+        this.getCredits = this.getCredits.bind(this);
+        this.getApprovedTransactions = this.getApprovedTransactions.bind(this);
+        this.calculateBalance = this.calculateBalance.bind(this);
+    }
+
+    getDebits(){
+        let debits = [];
+        let tempTransactions = this.state.transactions;
+        tempTransactions.forEach(transaction => {
+            let debitEntries = transaction.debitEntries;
+            debitEntries.forEach(entry => {
+                if(entry.account == "Accounts Receivable"){
+                    entry.date = transaction.date;
+                    entry.type = "debit";
+                    debits.push(entry);
+                }
+            });
+        });
+        
+        debits.forEach(debit => {
+            console.log(`ACCOUNT: ${debit.account} || AMOUNT: ${debit.amount} || DATE: ${debit.date} || TYPE: ${debit.type}`);
+        });
+
+        return debits;
+    }
+
+    getCredits(){
+        let credits = [];
+        let tempTransactions = this.state.transactions;
+        tempTransactions.forEach(transaction => {
+            let creditEntries = transaction.creditEntries;
+            creditEntries.forEach(entry => {
+                if(entry.account == "Accounts Receivable"){
+                    entry.date = transaction.date;
+                    entry.type = "credit";
+                    credits.push(entry);
+                }
+            });
+        });
+        
+        credits.forEach(credit => {
+            console.log(`ACCOUNT: ${credit.account} || AMOUNT: ${credit.amount} || DATE: ${credit.date} || TYPE: ${credit.type}`);
+        });
+
+        return credits;
+    }
+
+    calculateBalance(data){
+        let debitTotal = 0;
+        let creditTotal = 0;
+
+        for(let i = 0; i < data.length; i++){
+            if(data[i].type == "debit"){
+                debitTotal += data[i].amount;
+            } else if(data[i].type == "credit"){
+                creditTotal += data[i].amount;
+            }
+        }
+
+        let balance = Math.abs(debitTotal - creditTotal);
+        console.log(`BALANCE: ${balance}`);
+
+        if(debitTotal > creditTotal){
+            return {balanceType: "debit", balance: balance}
+        } else if(creditTotal > debitTotal){
+            return {balanceType: "credit", balance: balance}
+        } else {
+            return {balanceType: "none", balance: balance}
         }
     }
 
-    
-    componentDidMount(){
-        fetch('/transactions/approved')
-            .then(res => res.json())
-            .then(transactions => this.setState({ transactions: transactions}, () => {
-                console.log('Accounts fetched...', transactions);
-            }))
+    async getApprovedTransactions(){
+        await fetch('/transactions/approved')
+                .then(res => res.json())
+                .then(transactions => this.setState({ transactions: transactions}, () => {
+                    console.log('Accounts fetched...', transactions);
+                }))
+                .then( () => {
+                    let data = this.getDebits().concat(this.getCredits()); 
+                    this.setState({
+                        data: data
+                    }, () => {
+                        console.log(this.state.data);
+                    });  
+                }
+            )
+
+        let balance = this.calculateBalance(this.state.data);
+        this.setState({balance: balance});
+        console.log(`BALANCE TYPE: ${balance.balanceType} | BALANCE AMOUNT: ${balance.balance}`);
     }
+
+    componentDidMount(){
+        this.getApprovedTransactions();
+    }
+
 
     render(){
         return(
-            <div>
-                <h1>ACCOUNTS RECEIVABLE LEDGER</h1>
+            <div className="container">
+                <h1>ACCOUNTS RECEIVABLE</h1>
+                <ReactTable
+                data={this.state.data}
+                columns={[
+                    {
+                        Header: 'Date',
+                        id: 'date',
+                        accessor: d => d.date,
+                        Footer: (
+                            <div>
+                                {this.state.balance.balanceType == "none" ? <strong>{`BALANCE: ${(this.state.balance.balance).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`}</strong> : ""}
+                            </div>
+                        ) 
+                    },
+                    {
+                        Header: 'Debit',
+                        id: 'debit',
+                        accessor: d => <DisplayDebits type={d.type} amount={d.amount} />,
+                        Footer: (
+                            <div>
+                                {this.state.balance.balanceType == "debit" ? <strong>{`BALANCE: ${(this.state.balance.balance).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`}</strong> : ""}
+                            </div>
+                        ) 
+                        
+                    },
+                    {
+                        Header: 'Credit',
+                        id: 'credit',
+                        accessor: d => <DisplayCredits type={d.type} amount={d.amount} />,
+                        Footer: (
+                            <div>
+                                {this.state.balance.balanceType == "credit" ? <strong>{`BALANCE: ${(this.state.balance.balance).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`}</strong> : ""}
+                            </div>
+                        ) 
+                    }
+                ]}
+                defaultPageSize={10}
+                />
             </div>
         )
     }
 }
 
 export default AccountsReceivableLedger;
+
+function DisplayDebits(props){
+    if(props.type == "debit"){
+        return(
+            <div className="text-right">
+                {`${props.amount}.00`}
+            </div>
+        )   
+    } else{
+        return(
+            <div className="text-center">
+                {" - "}
+            </div>
+        )
+    }
+}
+
+function DisplayCredits(props){
+    if(props.type == "credit"){
+        return(
+            <div className="text-right">
+                {`${props.amount}.00`}
+            </div>
+        )
+    } else{
+        return(
+            <div className="text-center">
+                {" - "}
+            </div>
+        )
+    }
+}
+
+function DisplayDebitBalance(props){
+    if(props.balance.balanceType == "debit"){
+        return(
+            <div>
+                {`BALANCE: ${props.balance}`}
+            </div>
+        )
+    }
+}
+
+function DisplayCreditBalance(props){
+    if(props.balanceType == "credit"){
+        return(
+            <div>
+                {`BALANCE: ${props.balance}`}
+            </div>
+        )
+    }
+}
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -16,6 +16,25 @@ import Journalize from '../components/Journalize';
 
 import CashLedger from '../components/AccountLedgers/Cash';
 import AccountsReceivableLedger from '../components/AccountLedgers/AccountsReceivable';
+import PrepaidRentLedger from '../components/AccountLedgers/PrepaidRent';
+import PrepaidInsuranceLedger from '../components/AccountLedgers/PrepaidInsurance';
+import SuppliesLedger from '../components/AccountLedgers/Supplies';
+import OfficeEquipmentLedger from '../components/AccountLedgers/OfficeEquipment';
+import AccumulatedDepreciationLedger from '../components/AccountLedgers/AccumulatedDepreciation';
+import AccountsPayableLedger from '../components/AccountLedgers/AccountPayable';
+import SalariesPayableLedger from '../components/AccountLedgers/SalariesPayable';
+import UnearnedRevenueLedger from '../components/AccountLedgers/UnearnedRevenue';
+import ContributedCapitalLedger from '../components/AccountLedgers/ContributedCapital';
+import RetainedEarningsLedger from '../components/AccountLedgers/RetainedEarnings';
+import ServiceRevenueLedger from '../components/AccountLedgers/ServiceRevenue';
+import InsuranceExpenseLedger from '../components/AccountLedgers/InsuranceExpense';
+import DepreciationExpenseLedger from '../components/AccountLedgers/DepreciationExpense';
+import RentExpenseLedger from '../components/AccountLedgers/RentExpense';
+import SuppliesExpenseLedger from '../components/AccountLedgers/SuppliesExpense';
+import SalariesExpenseLedger from '../components/AccountLedgers/SalariesExpense';
+import TelephoneExpenseLedger from '../components/AccountLedgers/TelephoneExpense';
+import UtilitiesExpenseLedger from '../components/AccountLedgers/UtilitiesExpense';
+import AdvertisingExpenseLedger from '../components/AccountLedgers/AdvertisingExpense';
 
 function Routes() {
   return (
@@ -33,8 +52,29 @@ function Routes() {
       <Route exact path="/register" component={Register} />
       <Route exact path="/log" component={Log} />
 
+      {/* LEDGERS */}
       <Route exact path="/accounts/cash" component={CashLedger}/>
       <Route exact path="/accounts/accountsreceivable" component={AccountsReceivableLedger}/>
+      <Route exact path="/accounts/prepaidrent" component={PrepaidRentLedger}/>
+      <Route exact path="/accounts/prepaidinsurance" component={PrepaidInsuranceLedger}/>
+      <Route exact path="/accounts/supplies" component={SuppliesLedger}/>
+      <Route exact path="/accounts/officeequipment" component={OfficeEquipmentLedger}/>
+      <Route exact path="/accounts/accumulateddepreciation" component={AccumulatedDepreciationLedger}/>
+      <Route exact path="/accounts/accountspayable" component={AccountsPayableLedger}/>
+      <Route exact path="/accounts/salariespayable" component={SalariesPayableLedger}/>
+      <Route exact path="/accounts/unearnedrevenue" component={UnearnedRevenueLedger}/>
+      <Route exact path="/accounts/contributedcapital" component={ContributedCapitalLedger}/>
+      <Route exact path="/accounts/retainedearnings" component={RetainedEarningsLedger}/>
+      <Route exact path="/accounts/servicerevenue" component={ServiceRevenueLedger}/>
+      <Route exact path="/accounts/insuranceexpense" component={InsuranceExpenseLedger}/>
+      <Route exact path="/accounts/depreciationexpense" component={DepreciationExpenseLedger}/>
+      <Route exact path="/accounts/rentexpense" component={RentExpenseLedger}/>
+      <Route exact path="/accounts/suppliesexpense" component={SuppliesExpenseLedger}/>
+      <Route exact path="/accounts/salariesexpense" component={SalariesExpenseLedger}/>
+      <Route exact path="/accounts/telephoneexpense" component={TelephoneExpenseLedger}/>
+      <Route exact path="/accounts/utilitiesexpense" component={UtilitiesExpenseLedger}/>
+      <Route exact path="/accounts/advertisingexpense" component={AdvertisingExpenseLedger}/>
+
     </Switch>
   )
 }
