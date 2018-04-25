dd3dc3451b600e94b47836c96c14a366dde5ea08
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-04-25 14:50:06 -0400

    edited the mongoURI

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://zzaz3:ker1beros@ds229648.mlab.com:29648/skynet'
+  // mongoURI: 'mongodb://zzaz3:ker1beros@ds229648.mlab.com:29648/skynet'
+  mongoURI: process.env.MONGO_URI
   // mongoURI: 'mongodb://localhost/omegadot-dev'
 }
