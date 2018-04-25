b030d811f3a3cd3ac9c0a6a278de8e9538f11d25
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-04-25 16:14:49 -0400

    modified database config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-module.exports = {
-  mongoURI: 'mongodb://zzaz3:ker1beros@ds229648.mlab.com:29648/skynet'
+// module.exports = {
+//   mongoURI: 'mongodb://zzaz3:ker1beros@ds229648.mlab.com:29648/skynet'
   // mongoURI: process.env.MONGO_URI
   // mongoURI: 'mongodb://localhost/omegadot-dev'
-}
+// }
 
 if(process.env.NODE_ENV === "production"){
   module.exports = {mongoURI: "mongodb://zzaz3:ker1beros@ds259109.mlab.com:59109/skynet-prod"}
